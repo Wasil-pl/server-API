e1d8b3380f5d96dc52fa4f7655ce976cbd103d3f
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-06-26 10:24:45 +0200

    add online database

--- server.js
+++ server.js
@@ -13,17 +13,25 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
-app.use(cors());
-
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
+
+mongoose.connect(
+  'mongodb+srv://Wasil:EWHveTWdVRNpDpNh@cluster0.lvwrcbb.mongodb.net/music_festival?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: false,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -37,9 +45,6 @@ io.on('connection', (socket) => {
 });
 
 app.use((req, res) => res.status(404).json({ message: '404 not found...' }));
-
-mongoose.connect('mongodb://localhost:27017/music_festival', { useNewUrlParser: true, useFindAndModify: false });
-const db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Connected to the database');
